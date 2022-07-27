fb44ed0986dce4a3e3c4f3bb2865d93565b327c9
szeroki <seylio@outlook.com>
2022-07-27 21:58:14 +0200

    ioutil.ReadAll is depreacated

--- rest/rest.go
+++ rest/rest.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"strings"
 	"time"
@@ -47,7 +46,7 @@ func (c *Client) Request(endpoint, method string, data io.Reader, contentType st
 
 	var body []byte
 
-	body, err = ioutil.ReadAll(resp.Body)
+	body, err = io.ReadAll(resp.Body)
 
 	if err != nil {
 		return nil, err

62e4133f17619e3b62217651ffca2b1e5e5a51f7
Aslak <51925794+AslakOffi@users.noreply.github.com>
2023-06-17 00:32:17 +0200

    Optimisation of event handlers and use of constants for event names
    - Defined a common EventHandler interface for event handlers
    - Refactored event handlers to implement the EventHandler interface
    - Added error handling when creating events with error logs
    - Introduced constants for event names, making the code easier to read and maintain
    - Updated event references with corresponding constants
    
    These changes optimise event management in the Goscord, improve the robustness and flexibility of the code, and facilitate future maintenance.

--- goscord/gateway/auto_moderation_handler.go
+++ goscord/gateway/auto_moderation_handler.go
@@ -1,53 +1,62 @@
 package gateway
 
 import (
+	"log"
+
 	"github.com/Goscord/goscord/goscord/gateway/event"
 )
 
-type AutoModerationRuleCreateHandler struct{}
+const (
+	EventAutoModerationRuleCreate = "auto_moderation_rule_create"
+	EventAutoModerationRuleDelete = "auto_moderation_rule_delete"
+	EventAutoModerationRuleUpdate = "auto_moderation_rule_update"
+	EventAutoModerationActionExecution = "auto_moderation_action_execution"
+)
 
+type EventHandler interface {
+	Handle(s *Session, data []byte)
+}
+
+type AutoModerationRuleCreateHandler struct{}
 func (_ *AutoModerationRuleCreateHandler) Handle(s *Session, data []byte) {
 	ev, err := event.NewAutoModerationRuleCreate(s.rest, data)
-
 	if err != nil {
+		log.Println("Error when creating the event AutoModerationRuleCreate:", err)
 		return
 	}
 
-	s.Publish(event.EventAutoModerationRuleCreate, ev.Data)
+	s.Publish(EventAutoModerationRuleCreate, ev.Data)
 }
 
 type AutoModerationRuleDeleteHandler struct{}
-
 func (_ *AutoModerationRuleDeleteHandler) Handle(s *Session, data []byte) {
 	ev, err := event.NewAutoModerationRuleDelete(s.rest, data)
-
 	if err != nil {
+		log.Println("Error when creating the event AutoModerationRuleDelete:", err)
 		return
 	}
 
-	s.Publish(event.EventAutoModerationRuleDelete, ev.Data)
+	s.Publish(EventAutoModerationRuleDelete, ev.Data)
 }
 
 type AutoModerationRuleUpdateHandler struct{}
-
 func (_ *AutoModerationRuleUpdateHandler) Handle(s *Session, data []byte) {
 	ev, err := event.NewAutoModerationRuleUpdate(s.rest, data)
-
 	if err != nil {
+		log.Println("Error when creating the event AutoModerationRuleUpdate:", err)
 		return
 	}
 
-	s.Publish(event.EventAutoModerationRuleUpdate, ev.Data)
+	s.Publish(EventAutoModerationRuleUpdate, ev.Data)
 }
 
 type AutoModerationActionExecutionHandler struct{}
-
 func (_ *AutoModerationActionExecutionHandler) Handle(s *Session, data []byte) {
 	ev, err := event.NewAutoModerationActionExecution(s.rest, data)
-
 	if err != nil {
+		log.Println("Error when creating the event AutoModerationActionExecution:", err)
 		return
 	}
 
-	s.Publish(event.EventAutoModerationActionExecution, ev.Data)
+	s.Publish(EventAutoModerationActionExecution, ev.Data)
 }

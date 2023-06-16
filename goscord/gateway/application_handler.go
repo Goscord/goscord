c01a000ed77147d3ff06bae39d47464bf66e65a4
Aslak <51925794+AslakOffi@users.noreply.github.com>
2023-06-17 00:38:50 +0200

    Optimisation and improvement of the ApplicationCommandPermissionsUpdateHandler event handler
    - Addition of a constant for the event name, EventApplicationCommandPermissionsUpdate
    - Added error handling when the event is created, with error checking
    - Use of log.Println() to display errors in logs

--- goscord/gateway/application_handler.go
+++ goscord/gateway/application_handler.go
@@ -1,15 +1,24 @@
 package gateway
 
-import "github.com/Goscord/goscord/goscord/gateway/event"
+import (
+	"log"
+
+	"github.com/Goscord/goscord/goscord/gateway/event"
+)
+
+const (
+	EventApplicationCommandPermissionsUpdate = "application_command_permissions_update"
+)
 
 type ApplicationCommandPermissionsUpdateHandler struct{}
 
 func (_ *ApplicationCommandPermissionsUpdateHandler) Handle(s *Session, data []byte) {
 	ev, err := event.NewApplicationCommandPermissionsUpdate(s.rest, data)
-
 	if err != nil {
+		log.Println("Erreur lors de la création de l'événement ApplicationCommandPermissionsUpdate:", err)
 		return
 	}
 
-	s.Publish(event.EventApplicationCommandPermissionsUpdate, ev.Data)
+	s.Publish(EventApplicationCommandPermissionsUpdate, ev.Data)
 }
+
